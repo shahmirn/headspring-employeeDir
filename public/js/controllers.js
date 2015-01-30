61bc76be939e2a2048d27bab2c7f136ca887614d
shahmirn <shahmirn@gmail.com>
2015-01-30 08:22:46 -0600

    Added filtering

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,16 +2,29 @@ angular.module('hs.controllers', []).controller('HomeController', function(Emplo
   this.employees = Employee.query();
 
   this.gridOptions = {
+    enableFiltering: true,
     paginationPageSizes: [25, 50, 75],
     paginationPageSize: 25,
     columnDefs: [
       {
         name: 'picture',
-        cellTemplate: "<div class='ui-grid-cell-contents'><img class='avatar' src='{{row.entity.picture}}' /></div>"
+        cellTemplate: "<div class='ui-grid-cell-contents'><img class='avatar' src='{{row.entity.picture}}' /></div>",
+        enableFiltering: false
       },
       {
         name: 'name',
-        cellTemplate: "<div class='ui-grid-cell-contents'>{{row.entity.firstName}} {{row.entity.lastName}}</div>"
+        cellTemplate: "<div class='ui-grid-cell-contents'>{{row.entity.firstName}} {{row.entity.lastName}}</div>",
+        filter: {
+          condition: function(searchTerm, cellValue, row, column) {
+            searchTerm = searchTerm.toLowerCase();
+            var firstName = row.entity.firstName.toLowerCase();
+            var lastName = row.entity.lastName.toLowerCase();
+
+            return firstName.indexOf(searchTerm) === 0 || 
+              lastName.indexOf(searchTerm) === 0 || 
+              (firstName + ' ' + lastName).indexOf(searchTerm) === 0;
+          }
+        }
       },
       {
         name: 'jobTitle'
@@ -23,7 +36,8 @@ angular.module('hs.controllers', []).controller('HomeController', function(Emplo
         name: 'email'
       },
       {
-        name: 'phoneNumbers'
+        name: 'phoneNumbers',
+        enableFiltering: false
       }
     ],
     data: this.employees
