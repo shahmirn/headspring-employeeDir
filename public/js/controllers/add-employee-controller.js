cfbfa5283c6e490498ee34df8debae1a23ebb9c8
shahmirn <shahmirn@gmail.com>
2015-02-01 19:40:16 -0600

    Added edit functionality.

--- public/js/controllers/add-employee-controller.js
+++ public/js/controllers/add-employee-controller.js
@@ -1,5 +1,13 @@
 angular.module('hs.controllers').controller('AddEmployeeController', function($state, Employee, toaster) {
-    this.save = function() {
+    this.title = "Add Employee";
 
+    this.save = function() {
+        var newEmployee = new Employee(this.employee);
+        newEmployee.$save(function() {
+            toaster.pop('success', null, "Added employee successfully!");
+            $state.go('home');
+        }, function() {
+            toaster.pop('error', null, "Failed to add employee");
+        })
     };
 });
--- public/js/services.js
+++ public/js/services.js
@@ -1,14 +1,21 @@
 angular.module('hs.services', ['ngResource']).factory('Employee', function($resource) {
+
+    var transformRequest = function(data) {
+        if (data && data.phoneNumbers && typeof data.phoneNumbers === "string") {
+            data.phoneNumbers = data.phoneNumbers.replace(/\s/g, '').split(",");
+        }
+
+        return angular.toJson(data);
+    };
+
     return $resource('api/employees/:employeeId', {employeeId: '@_id'}, {
+        'save': {
+            method: 'POST',
+            transformRequest: transformRequest
+        },
         'update': {
             method:'PUT',
-            transformRequest: function(data) {
-                if (data && data.phoneNumbers && typeof data.phoneNumbers === "string") {
-                    data.phoneNumbers = data.phoneNumbers.replace(/\s/g, '').split(",");
-                }
-
-                return angular.toJson(data);
-            }
+            transformRequest: transformRequest
         }
     });
 }).factory('Login', function($resource) {
